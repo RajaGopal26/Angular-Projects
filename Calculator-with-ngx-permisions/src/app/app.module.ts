f7d6c605906ccea2ff67c7c1d4c648ffdb2dee68
MRajagopal26 <59041528+MRajagopal26@users.noreply.github.com>
2020-01-02 17:33:33 +0530

    based on user permisions -mrg

--- Calculator-with-ngx-permisions/src/app/app.module.ts
+++ Calculator-with-ngx-permisions/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { CalculatorComponent } from './calculator/calculator.component';
 import { NgxPermissionsModule } from 'ngx-permissions';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatTabsModule, MatMenuModule, MatSidenavModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,9 +24,18 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    NgxPermissionsModule.forRoot()
+    NgxPermissionsModule.forRoot(),
+    MatTabsModule,
+    MatSidenavModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+   exports: [
+    MatTabsModule,
+    MatMenuModule,
+    MatSidenavModule
+  ],
 })
 export class AppModule { }
--- Calculator-with-ngx-permisions/src/app/calculator/calculator.component.ts
+++ Calculator-with-ngx-permisions/src/app/calculator/calculator.component.ts
@@ -1,51 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxPermissionsService } from 'ngx-permissions';
 import { HttpClient } from '@angular/common/http';
+// import { MatToolbarModule } from '@angular/material';
 // import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-calculator',
-  templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+	selector: 'app-calculator',
+	templateUrl: './calculator.component.html',
+	styleUrls: ['./calculator.component.css']
 })
-export class CalculatorComponent implements OnInit  {
-	// registerForm: FormGroup;k
-	constructor(private permissionsService: NgxPermissionsService, private http: HttpClient) {}
+export class CalculatorComponent implements OnInit {
 
-	public num1:number;
-	public num2:number;
-	public result:number;
+	constructor(private permissionsService: NgxPermissionsService, private http: HttpClient) { }
+
+	public num1: number;
+	public num2: number;
+	public result: number;
 	public resultheading: any;
-	perm :String[];
-	ngOnInit(): void { 
-	// 	this.registerForm = this.formBuilder.group({
-	// 	num1: ['', Validators.required, Validators.minLength(6)],
-	// 	num2: ['', Validators.required, Validators.minLength(6)]
-		
-	// });
-		const perm =["ADMIN","EDITORS"];
-	 
+	public perm: string[];
+	ngOnInit(): void {
+
+		const perm = ["GUEST"];
+
 		this.permissionsService.loadPermissions(perm);
-		
-		this.http.get('/').subscribe((permissions) => {
-			//const perm = ["ADMIN", "EDITOR"]; example of permissions
-			this.permissionsService.loadPermissions(perm);
-		 })
-		 console.log("permission ",this.permissionsService.getPermissions);
-		 window.localStorage.setItem("privilege", JSON.stringify(perm))
-		 window.sessionStorage.setItem("privilege", JSON.stringify(perm))
-	  }
-	 
-	//   onChange() {
+	
+		console.log("permission ", this.permissionsService.getPermissions);
+		window.localStorage.setItem("privilege", JSON.stringify(perm))
+		window.sessionStorage.setItem("privilege", JSON.stringify(perm))
+	}
 
-    //     // stop here if form is invalid
-    //     if (this.registerForm.invalid) {
-	// 		alert('Invalid Input!! :-)')
-    //         return;
-    //     }
-
-    //     alert('SUCCESS!! :-)')
-    // }
 	sum() {
 		this.result = this.num1 + this.num2;
 		this.resultheading = this.num1 + ' + ' + this.num2;
@@ -60,17 +43,25 @@ export class CalculatorComponent implements OnInit  {
 		this.resultheading = this.num1 + ' x ' + this.num2;
 	}
 	div() {
-			this.result = this.num1 / this.num2;
-			this.resultheading = this.num1 + ' / ' + this.num2;
-		}
-		percent() {
-			this.result = this.num1 / 100  * this.num2;
-			this.resultheading = this.num1 + ' / ' + 100 + ' x '+ this.num2;
-		}
-		ngOnDestroy(): void {
-			// window.localStorage.removeItem("");
-			window.localStorage.clear;
-			window.sessionStorage.removeItem("privilege");
-			
-		  }
+		this.result = this.num1 / this.num2;
+		this.resultheading = this.num1 + ' / ' + this.num2;
+	}
+	percent() {
+		this.result = this.num1 / 100 * this.num2;
+		this.resultheading = this.num1 + ' / ' + 100 + ' x ' + this.num2;
+	}
+	user(){
+		let role=["EDITORS"];
+		this.permissionsService.loadPermissions(role);
+	}
+	admin(){
+		let role=["ADMIN"];
+		this.permissionsService.loadPermissions(role);
+	}
+	ngOnDestroy(): void {
+		// window.localStorage.removeItem("");
+		window.localStorage.clear;
+		window.sessionStorage.removeItem("privilege");
+
+	}
 }
